d4f43ec5b03d45bf55db13188e339205d052ba3e
sanjay <bhandari.sanjay82@gmail.com>
2021-11-08 23:22:44 +0530

    extra changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from "./screens/home/Home";
+import Details from "./screens/details/Details";
 import './index.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from "./screens/home/Home";
+import NotFound from './common/NotFound';
 
-ReactDOM.render(<BrowserRouter>
-    <Switch>
-      <Route path="/" exact component={Home}/>
-    </Switch>
-  </BrowserRouter>,document.getElementById('root'));
+
+ReactDOM.render(
+    <BrowserRouter>
+      {/* <Home /> */}
+      {/* <Details /> */}
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/detail" component={Details}/>
+        <Route component={NotFound}/>
+      </Switch>
+    </BrowserRouter>
+,
+  document.getElementById('root')
+);
+
