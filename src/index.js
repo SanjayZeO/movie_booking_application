796e7b186b9492aebedca735a8a908ef8d64cb5a
sanjay <bhandari.sanjay82@gmail.com>
2021-11-08 22:50:21 +0530

    Checkpoint 3

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from "./screens/home/Home";
 
-ReactDOM.render(
-  <React.StrictMode>
-    
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Home}/>
+    </Switch>
+  </BrowserRouter>,document.getElementById('root'));
